c6d5ce35bd3d1a278092eb5f6723683636c170d6
swuttik <svatoslavgetun@gmail.com>
2026-09-09 20:58:47 +0300

    feat: libs/errors integrated into screens

--- src/components/screens/register/hooks/useRegister.ts
+++ src/components/screens/register/hooks/useRegister.ts
@@ -1,14 +1,13 @@
 import { zodResolver } from "@hookform/resolvers/zod"
-import type { AxiosError } from "axios"
 import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
 
-import { errorCatch } from "@/api/helper"
 import { service } from "@/api/instance"
 import { Toast } from "@/components/ui"
 import { register } from "@/libs/auth"
 import { AppRoutes } from "@/libs/constants"
 import { RegisterFormFields, registerScheme } from "@/libs/schemes"
+import { errorCatch } from "@/libs/utils"
 import { useAuthStore } from "@/store/auth"
 
 export const useRegisterPage = () => {
@@ -36,12 +35,11 @@ export const useRegisterPage = () => {
 
 			setTimeout(() => replace(AppRoutes.MENU), 5000)
 		} catch (error) {
-			console.log(error)
-			const { message, cause } = errorCatch(error as AxiosError)
+			const e = errorCatch(error)
 			Toast.show({
 				type: "error",
 				text1: "Не удалось зарегистрироваться!",
-				text2: cause
+				text2: e.message
 			})
 		}
 	}
