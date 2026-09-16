70902060dc5cc75695bdde953fd1c84eb9b5026e
swuttik <svatoslavgetun@gmail.com>
2026-09-17 01:13:15 +0300

    feat: avatar query-client API

--- src/libs/query/hooks/use-sessions.ts
+++ src/libs/query/hooks/use-sessions.ts
@@ -1,9 +1,10 @@
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
 import { service } from "@/api/instance"
+import { ms } from "@/libs/utils"
 import { useAuthStore } from "@/store/auth"
 
-import { QueryKeys } from "../keys"
+import { QueryKeys } from "../keys";
 
 export const useSessions = () => {
 	const state = useAuthStore(state => state.status)
@@ -11,16 +12,22 @@ export const useSessions = () => {
 	const { isLoading, ...other } = useQuery({
 		queryKey: QueryKeys.sessions.all,
 		queryFn: service.getSessions,
-		enabled: state === "authenticated"
+		enabled: state === "authenticated",
+		staleTime: ms("5min"),
+		gcTime: ms("10min")
 	})
 
 	return { isLoading: isLoading || state === "loading", ...other }
 }
 
 export const useDeleteSession = (id: string) => {
+	const client = useQueryClient()
+
 	return useMutation({
 		mutationKey: QueryKeys.sessions.delete(id),
-		mutationFn: () => service.deleteSessionsId(id)
+		mutationFn: () => service.deleteSessionsId(id),
+		onSuccess: () =>
+			client.invalidateQueries({ queryKey: QueryKeys.sessions.all })
 	})
 }
 
