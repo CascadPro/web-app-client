ed3c77d21569b94d00d2d36cd58d9940ee7fd16e
swuttik <svatoslavgetun@gmail.com>
2026-09-17 15:48:24 +0300

    feat: query-client user integrated

--- src/components/screens/my_profile/profile.tsx
+++ src/components/screens/my_profile/profile.tsx
@@ -1,23 +1,22 @@
 "use client"
 
 import { PullToRefresh } from "@/components/pwa/pwa-refresh-control";
-import { refreshUser } from "@/libs/auth";
 
 import { AccountInformation } from "./components/account-information";
-import { ProfileHeader } from "./components/header/profile-header"
+import { ProfileHeader } from "./components/header/profile-header";
 import { ProfileInformation } from "./components/profile-information";
 import { ProfileLoading } from "./components/profile-loading";
 import { useProfilePage } from "./hooks/useProfilePage";
 
 export const MyProfileScreen = () => {
-	const { status, user, fullName, avatarUser } = useProfilePage()
+	const { status, user, refetchUser, fullName, avatarUser } = useProfilePage()
 
 	if (status === "loading") {
 		return <ProfileLoading />
 	}
 
 	const handleRefresh = async () => {
-		await refreshUser()
+		await refetchUser()
 	}
 
 	return (
